42fb4858cfc9544f1155eff13ccbcb1bc63c2150
Klaus Jrgensen <klaus@blackwoodseven.com>
2017-05-06 03:06:30 +0200

    catch all errors

--- src/MessageQueueService.php
+++ src/MessageQueueService.php
@@ -20,6 +20,10 @@ class MessageQueueService
             $message = json_decode($payload);
             try {
                 $this->app['jsonschema']['message']->validate($message);
+                $message->status = $app['mail']->send($message);
+                $message->uts = $app['time'];
+                $app['message.repo']->save($message);
+                $app['console.output']->writeln(json_encode($message));
             } catch (\JsonSchemaValidation\ValidationException $e) {
                 $app['console.output']->writeln($e->getViolation());
                 return false;
@@ -27,10 +31,6 @@ class MessageQueueService
                 $app['console.output']->writeln($e->getMessage());
                 return false;
             }
-            $message->status = $app['mail']->send($message);
-            $message->uts = $app['time'];
-            $app['message.repo']->save($message);
-            $app['console.output']->writeln(json_encode($message));
         });
         $this->app['mq.client']->wait();
     }
